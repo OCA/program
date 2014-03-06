76ab06644adf00df04d62e07ab77fe88c4866927
david Cormier <david.cormier@savoirfairelinux.com>
2014-03-06 16:01:33 -0500

    [NEW] Add program indicator module

---
+++ program_indicator/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2013 Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################
+
+{
+    'name': 'Program Indicator',
+    'version': '0.1',
+    'author': 'Savoir-faire Linux',
+    'maintainer': 'Savoir-faire Linux',
+    'website': 'http://www.savoirfairelinux.com',
+    'category': 'Customer Relationship Management',
+    'summary': 'Program Indicator',
+    'description': """
+Program Indicator
+==================
+
+Contributeurs
+-------------
+* David Cormier (david.cormier@savoirfairelinux.com)
+""",
+    'depends': [
+        'program'
+    ],
+    'external_dependencies': {},
+    'data': [
+        'program_indicator_view.xml',
+    ],
+    'demo': [
+    ],
+    'test': [],
+    'installable': True,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
